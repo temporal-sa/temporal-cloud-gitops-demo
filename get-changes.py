c358d5a23a0aa2abebe558dd4c0c343167656c04
Brendan Myers <brendan.myers@temporal.io>
2025-01-31 14:55:27 +1100

    feat: users

--- get-changes.py
+++ get-changes.py
@@ -1,5 +1,6 @@
 import argparse
 import yaml
+import json
 
 parser = argparse.ArgumentParser(description="Find the changes since the last commit.")
 parser.add_argument("--prev", type=str, required=True, help="Path to previous YAML revision")
@@ -10,6 +11,8 @@ args = parser.parse_args()
 with open(args.prev) as prev_file, open(args.curr) as curr_file:
     prev_data = yaml.safe_load(prev_file)
     curr_data = yaml.safe_load(curr_file)
+
+    # Namespaces
 
     prev_namespaces = {ns["name"]: ns for ns in prev_data.get("namespaces", [])}
     curr_namespaces = {ns["name"]: ns for ns in curr_data.get("namespaces", [])}
@@ -24,7 +27,29 @@ with open(args.prev) as prev_file, open(args.curr) as curr_file:
         for name in prev_namespaces if name not in curr_namespaces
     ]
 
-    import json
     print(f"::set-output name=added_namespaces::{json.dumps(added_namespaces)}")
     print(f"::set-output name=removed_namespaces::{json.dumps(removed_namespaces)}")
-          
+    
+    # Users
+
+    prev_users = {ns["email"]: ns for ns in prev_data.get("users", [])}
+    curr_users = {ns["email"]: ns for ns in curr_data.get("users", [])}
+
+    added_users = [
+        {"email": email, "role": curr_users[email]["role"]}
+        for email in curr_users if email not in prev_users
+    ]
+
+    modified_users = [
+        {"email": email, "role": curr_users[email]["role"]}
+        for email in prev_users if email in curr_users and prev_users[email] != curr_users[email]
+    ]
+
+    removed_users = [
+        {"email": email, "role": prev_users[email]["role"]}
+        for email in prev_users if email not in curr_users
+    ]
+
+    print(f"::set-output name=added_users::{json.dumps(added_users)}")
+    print(f"::set-output name=modified_users::{json.dumps(modified_users)}")
+    print(f"::set-output name=removed_users::{json.dumps(removed_users)}")
